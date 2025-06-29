de1bcbc50194a13adb4bd985ce38dde62f633242
Jericho Serrano <118679068+jericho1050@users.noreply.github.com>
2025-06-29 23:03:28 +0800

    fix: correct render.yaml and redis connection logic

--- src/common/env.ts
+++ src/common/env.ts
@@ -22,6 +22,7 @@ export default {
   CONNECTIONS_SECRET: process.env.CONNECTIONS_SECRET as string,
   JWT_CONNECTIONS_SECRET: process.env.JWT_CONNECTIONS_SECRET as string,
   DATABASE_URL: process.env.DATABASE_URL as string,
+  REDIS_URL: process.env.REDIS_URL as string,
   REDIS_HOST: process.env.REDIS_HOST as string,
   REDIS_PORT: parseInt(process.env.REDIS_PORT as string),
   REDIS_PASS: process.env.REDIS_PASS as string,
